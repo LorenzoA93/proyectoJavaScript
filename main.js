eba4bdba1c1d8441091cb0c5a2a19dc3638563b3
LorenzoA93 <lorenzoestebanaraya@gmail.com>
2022-11-27 13:06:58 -0300

    27/11/2022

--- main.js
+++ main.js
@@ -4,20 +4,25 @@ let nombreDelSegundoOperario = prompt ("Ingrese el nombre del Segundo Operario")
 let talleDelSegundoOperario = parseInt(prompt ("Ingrese el talle del Segundo Operario"));
 let nombreDelTercerOperario = prompt ("Ingrese el nombre del Tercer Operario");
 let talleDelTercerOperario = parseInt(prompt ("Ingrese el talle del Tercer Operario"));
-console.log ("El nombre del primer operario es: " + nombreDelPrimerOperario + " y su talle es " + talleDelPrimerOperario);
-console.log ("El nombre del segundo operario es: " + nombreDelSegundoOperario + " y su talle es " + talleDelSegundoOperario);
-console.log ("El nombre del tercer operario es: " + nombreDelTercerOperario + " y su talle es " + talleDelTercerOperario);
+let trabajadores = {
+    primero: nombreDelPrimerOperario,
+    segundo: nombreDelSegundoOperario,
+    tercero: nombreDelTercerOperario,
+};
+for (let clave in trabajadores){
+    console.log("El nombre del operario es: " + trabajadores[clave])
+};
 function promedio(valorA, valorB, valorC) {
     let resultado = (valorA + valorB +valorC) / 3;
     return resultado;
-}
+};
 let resultadoPromedio = promedio (talleDelPrimerOperario, talleDelSegundoOperario, talleDelTercerOperario);
-switch (resultadoPromedio) {
-    case resultadoPromedio > 60:
-        return ccccc
-    case 60 > resultadoPromedio > 40:
-        return jjjj
-    case resultadoPromedio < 40:
-        return ttttt
-}
-console.log (resultadoPromedio);
+function calculoPromedio(resultadoPromedio) {
+    if (resultadoPromedio > 60) {
+        return "El resultado es demaciado alto, prueba ingresando de nuevo los datos"
+    }else{
+        return "El resultado es correcto, se enviará la orden a Recursos Humanos"
+    }
+};
+let promedios = calculoPromedio (resultadoPromedio);
+console.log(promedios);
